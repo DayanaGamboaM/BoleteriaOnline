cba1831c2c6acab861dbd0610a33a53093ac91f1
AndreyBV5 <127549686+AndreyBV5@users.noreply.github.com>
2023-05-21 16:11:12 -0600

    Correcciones

--- boleteria/components/componentsQR/Hero.tsx
+++ boleteria/components/componentsQR/Hero.tsx
@@ -4,7 +4,7 @@ import { Button} from 'react-bootstrap';
 const Hero = () => {
   return (
     <div
-        className='HeroContainer'><Button className='HeroButtonContainer' variant="dark"><span>Validar</span></Button>
+        className='HeroContainer'><Button className='HeroButtonContainer' variant="light"><span className="text-custom fs-5 fw-bold">Validar</span></Button>
     </div>
   )
 }
