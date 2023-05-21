aec96510552aa7e483407c8b54d1fa2f9104e5ba
DayanaGamboaM <mongeg1715@gmail.com>
2023-05-21 14:20:49 -0600

    component seats

--- boleteria/src/pages/paymentGateway.tsx
+++ boleteria/src/pages/paymentGateway.tsx
@@ -4,6 +4,7 @@ import Boarding from '../../components/componentsPaymentGateway/Boarding';
 import NavBar from '../../components/NavBar';
 import TravelDate from '../../components/componentsPaymentGateway/TravelDate';
 import Hour from '../../components/componentsPaymentGateway/Hour';
+import Seats from '../../components/componentsPaymentGateway/Seats';
 const Home = ()=>{
 
     return(
@@ -12,6 +13,7 @@ const Home = ()=>{
             <Boarding/>
             <TravelDate/>
             <Hour/>
+            <Seats/>
         </>
     )
 }
