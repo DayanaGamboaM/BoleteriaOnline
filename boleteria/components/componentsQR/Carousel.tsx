8a53adf43f4010d1312524b41d8a214b818f9c72
AndreyBV5 <127549686+AndreyBV5@users.noreply.github.com>
2023-05-21 10:44:34 -0600

    Responsive Image and Hero
    se implementó por medio de css que la imagen que será un carrusel en el futuro sea responsivo y tambien se hizo responsivo el div que contiene los datos de Hero

--- boleteria/components/componentsQR/Carousel.tsx
+++ boleteria/components/componentsQR/Carousel.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 const Carousel = () => {
   return (
     <div>
-      <Image className='ImgContainer' src={Banner} alt="Banner" width={1366} height={350}/>
+      <Image className='ImgContainer' src={Banner} alt="Banner" width={1366} height={250}/>
     </div>
   )
 }
