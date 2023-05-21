c9265cc0c9ab7ec5a98ae1c242821801c8caed44
CristoferBV <70454042+CristoferBV@users.noreply.github.com>
2023-05-20 18:38:37 -0600

    testing image for banner

--- boleteria/src/pages/account.tsx
+++ boleteria/src/pages/account.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
-import Layout from '../../components/Layout';
-const Home = ()=>{
+import React from "react";
+import Layout from "../../components/Layout";
+import Banner from "../../public/bus-banner.jpg";
+import Image from "next/image";
+const Home = () => {
+  return (
+    <Layout>
+      <div className="container-account">
+        <div className="banner">
+          <Image
+            src={Banner}
+            alt="Banner"
+            placeholder="blur"
+            width={1280}
+            height={400}
+          />
+        </div>
+      </div>
+    </Layout>
+  );
+};
 
-    return(
-
-        <Layout>
-            <h1>Account</h1>
-        </Layout>
-    )
-}
-
-export default Home ; 
+export default Home;
