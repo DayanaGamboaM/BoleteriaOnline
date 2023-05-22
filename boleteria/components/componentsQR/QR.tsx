96ced05a806042aa0b70c05477f56fc6bd9b10b6
AndreyBV5 <127549686+AndreyBV5@users.noreply.github.com>
2023-05-21 18:40:54 -0600

    Data QR Div
    se añadio un nuevo div para añadir un dato de estado

--- boleteria/components/componentsQR/QR.tsx
+++ boleteria/components/componentsQR/QR.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const QR = () => {
-  return (
-    <div className='QrContainer'></div>
-  )
+    return (
+        <div className='QrContainer'><div className='QRdataContainer'></div><div className='StateContainer'></div><h5>Estado</h5></div>
+    )
 }
 
 export default QR
