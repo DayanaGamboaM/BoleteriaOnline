a130ded5589395eef4241a9255567fef10b9f2e8
AndreyBV5 <127549686+AndreyBV5@users.noreply.github.com>
2023-05-23 15:16:38 -0600

    Carousel QR
    se implentó el carrusel para añadir los QR por el momento solo serán imagenes pero lo idea es hacer que se generen QR aleatorios

--- boleteria/components/componentsQR/QR.tsx
+++ boleteria/components/componentsQR/QR.tsx
@@ -1,12 +1,43 @@
 import React from "react";
 import Image from "next/image";
-import QRlogo from "../QR-1.png"
+import Carousel from 'react-bootstrap/Carousel';
+import imagen1 from "../QR-1.png"
+import imagen2 from "../QR-2.jpg"
+import imagen3 from "../QR-3.jpg"
 
 const QR = () => {
   return (
     <div className="QrContainer">
       <div className="QRdataContainer"></div>
-      <Image className='QRLogoContainer' src={QRlogo} alt="Logo" width={200} height={200}/>
+      <Carousel className="QrCarouselContainer" indicators = {false}>
+        <Carousel.Item interval={100000}>
+          <Image
+            className="d-block w-100"
+            src= {imagen1}
+            alt="First slide"
+            width={200}
+            height={200}
+          />
+        </Carousel.Item>
+        <Carousel.Item interval={100000}>
+          <Image
+            className="d-block w-100"
+            src= {imagen2}
+            alt="Second slide"
+            width={200}
+            height={200}
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <Image
+            className="d-block w-100"
+            src= {imagen3}
+            alt="Third slide"
+            width={200}
+            height={200}
+          />
+        </Carousel.Item>
+      </Carousel>
       <div className="StateContainer"></div>
       <h5>Estado</h5>
     </div>
