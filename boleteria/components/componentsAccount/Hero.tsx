e36e3685df06608dc8c94a34e0b16789cac12493
CristoferBV <70454042+CristoferBV@users.noreply.github.com>
2023-05-23 14:11:51 -0600

    Hero

---
+++ boleteria/components/componentsAccount/Hero.tsx
@@ -0,0 +1,37 @@
+import Image from 'next/image'
+import React from 'react'
+import Banner from "../../public/bus-banner.jpg";
+import UserProfile from "../../public/usuario.png";
+
+const Hero = () => {
+  return (
+    <div className="container-fluid p-0">
+      <div className="row">
+        <div className="col">
+          <div className="container-account">
+            <div className="banner">
+              <div className="banner-image-container position-relative">
+                <Image
+                  src={Banner}
+                  alt="Banner"
+                  className="img-fluid banner-image"
+                />
+                <div className="user-profile position-absolute top-50 start-50 translate-middle">
+                  <Image
+                    src={UserProfile}
+                    alt="user profile"
+                    className="user-image rounded-circle"
+                    width={100}
+                    height={100}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero
--- boleteria/src/pages/account.tsx
+++ boleteria/src/pages/account.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import Layout from "../../components/Layout";
-import Banner from "../../public/bus-banner.jpg";
-import UserProfile from "../../public/usuario.png";
-import Image from "next/image";
-import UserPhoto from "../../components/componentsAccount/UserPhoto";
 import BodyAccount from "../../components/componentsAccount/BodyAccount";
+import Hero from "../../components/componentsAccount/Hero";
 const Home = () => {
   return (
     <Layout>
       <div>
-        <UserPhoto/>
+        <Hero/>
         <BodyAccount/>
       </div>
     </Layout>
