e48f1e25667748b261bbaa20a2e6590001df4284
CristoferBV <70454042+CristoferBV@users.noreply.github.com>
2023-05-23 20:52:22 -0600

    changes in Hero

--- boleteria/components/componentsAccount/Hero.tsx
+++ boleteria/components/componentsAccount/Hero.tsx
@@ -1,37 +1,27 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 import Banner from "../../public/bus-banner.jpg";
 import UserProfile from "../../public/usuario.png";
 
 const Hero = () => {
   return (
-    <div className="container-fluid p-0">
-      <div className="row">
-        <div className="col">
-          <div className="container-account">
-            <div className="banner">
-              <div className="banner-image-container position-relative">
-                <Image
-                  src={Banner}
-                  alt="Banner"
-                  className="img-fluid banner-image"
-                />
-                <div className="user-profile">
-                  <Image
-                    src={UserProfile}
-                    alt="user profile"
-                    className="user-image rounded-circle"
-                    width={100}
-                    height={100}
-                  />
-                </div>
-              </div>
-            </div>
+    <div className="container-account">
+      <div className="banner">
+        <div className="banner-image-container position-relative">
+          <Image src={Banner} alt="Banner" className="img-fluid banner-image" />
+          <div className="user-profile">
+            <Image
+              src={UserProfile}
+              alt="user profile"
+              className="user-image rounded-circle"
+              width={100}
+              height={100}
+            />
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Hero
+export default Hero;
