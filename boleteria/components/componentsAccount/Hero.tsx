3b0ef80a4bc4e7503b66041dfba210e0495c1e28
CristoferBV <70454042+CristoferBV@users.noreply.github.com>
2023-05-24 16:51:47 -0600

    changes

--- boleteria/components/componentsAccount/Hero.tsx
+++ boleteria/components/componentsAccount/Hero.tsx
@@ -13,7 +13,7 @@ const Hero = () => {
             <Image
               src={UserProfile}
               alt="user profile"
-              className="user-image rounded-circle"
+              className="user-image"
               width={100}
               height={100}
             />
