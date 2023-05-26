f6fb37791f2b377d53cd7ec488abc6d84c92ac3a
AndreyBV5 <127549686+AndreyBV5@users.noreply.github.com>
2023-05-25 19:31:43 -0600

    Style Button Tickets
    se agregaron estilos a los botones

--- boleteria/components/componentsMyTickets/Tickets.tsx
+++ boleteria/components/componentsMyTickets/Tickets.tsx
@@ -11,28 +11,30 @@ const Tickets = () => {
       <h5 className="card-text mb-3">Fecha salida 17-07-2023</h5>
       <h5 className="card-text">Hora salida 08:00 AM</h5>
       <h5 className="card-text">Precio: 11500</h5>
-      <Button className="btn btn-primary" style={{boxShadow: '0 2px 2px rgba(0, 0, 0, 0.4)', marginRight: '10px', flex: '0 0 300px', maxWidth: '100%'}}>Imprimir</Button>
-      <Button className="btn btn-primary" style={{boxShadow: '0 2px 2px rgba(0, 0, 0, 0.4)', marginRight: '10px', flex: '0 0 300px', maxWidth: '100%'}}>Ver Tiquetes</Button>
+      <Button className="btn btn-costume" variant='ligth' style={{boxShadow: '0 2px 2px rgba(0, 0, 0, 0.4)', marginTop:'20px', marginRight: '10px', flex: '0 0 300px', maxWidth: '100%', borderRadius: 20, background:'#D9D9D9', color: 'black'}}>Imprimir</Button>
+      <Button className="btn btn-costume" variant='ligth' style={{boxShadow: '0 2px 2px rgba(0, 0, 0, 0.4)', marginTop:'20px', marginRight: '10px', flex: '0 0 300px', maxWidth: '100%', borderRadius: 20, background:'#D9D9D9', color: 'black'}}>Tiquetes</Button>
     </div>
   </div>
   <div className="card mt-5 text-center" style={{borderRadius: '10px', width: '100%', maxWidth: '900px', background: '#3C6E71', color: 'white', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)'}}>
     <div className="card-body">
       <h5 className="card-title mb-3">UNA-TRANSPORTE UNA</h5>
-      <h5 className="card-subtitle mb-3">CANOAS-SAN JOSE</h5>
-      <h5 className="card-text mb-3">Fecha salida 17-07-2023</h5>
-      <h5 className="card-text">Hora salida 08:00 AM</h5>
-      <Button className="btn btn-primary" style={{boxShadow: '0 2px 2px rgba(0, 0, 0, 0.4)', marginRight: '10px', flex: '0 0 300px', maxWidth: '100%'}}>Imprimir</Button>
-      <Button className="btn btn-primary" style={{boxShadow: '0 2px 2px rgba(0, 0, 0, 0.4)', marginRight: '10px', flex: '0 0 300px', maxWidth: '100%'}}>Ver Tiquetes</Button>
+      <h5 className="card-subtitle mb-3">SAN JOSE-CANOAS</h5>
+      <h5 className="card-text mb-3">Fecha salida 20-07-2023</h5>
+      <h5 className="card-text">Hora salida 05:00 AM</h5>
+      <h5 className="card-text">Precio: 11500</h5>
+      <Button className="btn btn-costume" variant='ligth' style={{boxShadow: '0 2px 2px rgba(0, 0, 0, 0.4)', marginTop:'20px', marginRight: '10px', flex: '0 0 300px', maxWidth: '100%', borderRadius: 20, background:'#D9D9D9', color: 'black'}}>Imprimir</Button>
+      <Button className="btn btn-costume" variant='ligth' style={{boxShadow: '0 2px 2px rgba(0, 0, 0, 0.4)', marginTop:'20px', marginRight: '10px', flex: '0 0 300px', maxWidth: '100%', borderRadius: 20, background:'#D9D9D9', color: 'black'}}>Tiquetes</Button>
     </div>
   </div>
   <div className="card mt-5 text-center flex-wrap" style={{borderRadius: '10px', width: '100%', maxWidth: '900px', background: '#3C6E71', color: 'white', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)'}}>
     <div className="card-body">
       <h5 className="card-title mb-3">UNA-TRANSPORTE UNA</h5>
       <h5 className="card-subtitle mb-3">CANOAS-SAN JOSE</h5>
-      <h5 className="card-text mb-3">Fecha salida 17-07-2023</h5>
-      <h5 className="card-text">Hora salida 08:00 AM</h5>
-      <Button className="btn btn-primary" style={{boxShadow: '0 2px 2px rgba(0, 0, 0, 0.4)', marginRight: '10px', flex: '0 0 300px', maxWidth: '100%'}}>Imprimir</Button>
-      <Button className="btn btn-primary" style={{boxShadow: '0 2px 2px rgba(0, 0, 0, 0.4)', marginRight: '10px', flex: '0 0 300px', maxWidth: '100%'}}>Ver Tiquetes</Button>
+      <h5 className="card-text mb-3">Fecha salida 01-12-2023</h5>
+      <h5 className="card-text">Hora salida 10:00 AM</h5>
+      <h5 className="card-text">Precio: 10500</h5>
+      <Button className="btn btn-costume" variant='ligth' style={{boxShadow: '0 2px 2px rgba(0, 0, 0, 0.4)', marginTop:'20px', marginRight: '10px', flex: '0 0 300px', maxWidth: '100%', borderRadius: 20, background:'#D9D9D9', color: 'black'}}>Imprimir</Button>
+      <Button className="btn btn-costume" variant='ligth' style={{boxShadow: '0 2px 2px rgba(0, 0, 0, 0.4)', marginTop:'20px', marginRight: '10px', flex: '0 0 300px', maxWidth: '100%', borderRadius: 20, background:'#D9D9D9', color: 'black'}}>Tiquetes</Button>
     </div>
   </div>
 </div>
