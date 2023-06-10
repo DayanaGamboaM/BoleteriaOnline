6f4223cd5568ba8ae236938f7d7f283fbfeeb6db
Maria-Chacon <mariajchm1312@gmail.com>
2023-06-09 22:56:39 -0600

    user authentication
    Co-Authored-By: DayanaGamboaM <89230613+DayanaGamboaM@users.noreply.github.com>

--- boleteria/src/pages/login.tsx
+++ boleteria/src/pages/login.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import Login from '../../components/componentsLogin/Login';
 import SignUp from '../../components/componentsLogin/Register';
 
-const Home = () => {
+const Loginn = () => {
   return (
       <Login />
   );
 }
 
-export default Home;
+export default Loginn;
