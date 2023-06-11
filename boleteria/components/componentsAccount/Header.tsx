d085534b3e0b8f3c9d05758194c4207ba69803f6
CristoferBV <70454042+CristoferBV@users.noreply.github.com>
2023-06-10 18:15:47 -0600

    Changes in header
    I implement a button for browse in file explorer for choose a image

--- boleteria/components/componentsAccount/Header.tsx
+++ boleteria/components/componentsAccount/Header.tsx
@@ -1,12 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Image from "next/image";
-import UserProfile from "../../public/usuario.png";
+import UserProfile from "../../public/user.png";
 
 const Header = () => {
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
 
-  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
+  const handleImageChange = () => {
+    const file = fileInputRef.current?.files?.[0];
     if (file) {
       const imageUrl = URL.createObjectURL(file);
       setSelectedImage(imageUrl);
@@ -14,42 +15,93 @@ const Header = () => {
   };
 
   const handleImageClick = () => {
-    const fileInput = document.getElementById("fileInput");
-    if (fileInput) {
-      fileInput.click();
-    }
+    fileInputRef.current?.click();
   };
 
   const renderImage = () => {
     if (selectedImage) {
       return (
-        <Image
-          className="img-fluid"
-          src={selectedImage}
-          alt="Selected Image"
-          width={100}
-          height={100}
-          onClick={handleImageClick}
-        />
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+          <div
+            style={{
+              width: "100px",
+              height: "100px",
+              borderRadius: "50%",
+              overflow: "hidden",
+            }}
+          >
+            <Image
+              className="img-fluid"
+              src={selectedImage}
+              alt="Selected Image"
+              width={100}
+              height={100}
+            />
+          </div>
+          <div style={{ marginTop: "10px" }}>
+          <button
+                onClick={handleImageClick}
+                style={{
+                  border: "none",
+                  background: "black",
+                  color: "white",
+                  cursor: "pointer",
+                  padding: "8px 20px",
+                  borderRadius:"1em",
+                  boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.2)",
+                  fontSize: "14px",
+                  fontWeight: "900",
+                  marginLeft: "-25px",
+                  textTransform:"uppercase"
+                }}
+              >
+                Buscar Imagen
+              </button>
+          </div>
+        </div>
       );
     } else {
       return (
-        <label htmlFor="fileInput">
-          <Image
-            className="img-fluid"
-            src={UserProfile}
-            alt="Initial Image"
-            width={100}
-            height={100}
-          />
-          <input
-            id="fileInput"
-            type="file"
-            accept="image/*"
-            onChange={handleImageChange}
-            style={{ display: "none" }}
-          />
-        </label>
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+          <label htmlFor="fileInput">
+            <div
+              style={{
+                width: "100px",
+                height: "100px",
+                borderRadius: "50%",
+                overflow: "hidden",
+              }}
+            >
+              <Image
+                className="img-fluid"
+                src={UserProfile}
+                alt="Initial Image"
+                width={100}
+                height={100}
+              />
+            </div>
+            <div style={{ marginTop: "10px" }}>
+            <button
+                onClick={handleImageClick}
+                style={{
+                  border: "none",
+                  background: "black",
+                  color: "white",
+                  cursor: "pointer",
+                  padding: "8px 20px",
+                  borderRadius:"1em",
+                  boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.2)",
+                  fontSize: "14px",
+                  fontWeight: "900",
+                  marginLeft: "-25px",
+                  textTransform:"uppercase"
+                }}
+              >
+                Buscar Imagen
+              </button>
+            </div>
+          </label>
+        </div>
       );
     }
   };
@@ -63,8 +115,16 @@ const Header = () => {
               className="card-block bg-white mb-3 d-flex flex-row flex-wrap align-items-center justify-content-between"
               style={{ width: "1400px", maxWidth: "100%" }}
             >
-              <div className="text-center mb-3 mb-md-0 mx-auto ">
+              <div className="text-center mb-3 mb-md-0 mx-auto">
                 {renderImage()}
+                <input
+                  id="fileInput"
+                  type="file"
+                  accept="image/*"
+                  ref={fileInputRef}
+                  style={{ display: "none" }}
+                  onChange={handleImageChange}
+                />
               </div>
             </div>
           </div>
@@ -75,11 +135,3 @@ const Header = () => {
 };
 
 export default Header;
-
-
-
-
-
-
-
-
