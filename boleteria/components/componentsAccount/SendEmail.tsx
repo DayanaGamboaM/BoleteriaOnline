2629e1868c4eb141e53c810cbd4c6d5ae41f2ee2
CristoferBV <70454042+CristoferBV@users.noreply.github.com>
2023-06-13 19:44:53 -0600

    chnages in sendEmail
    Solution of wrong with button and datepicker

--- boleteria/components/componentsAccount/SendEmail.tsx
+++ boleteria/components/componentsAccount/SendEmail.tsx
@@ -7,10 +7,10 @@ const SendEmail = () => {
   const [toggle, setToggle] = useState(false)
 
   return (
-    <div className="container" style={{marginTop: "-70px"}}>
+    <div className="container" style={{}}>
       <div className="card-section">
         <div className="container">
-          <div className="card-email bg-white d-flex flex-row flex-wrap align-items-center justify-content-between" style={{position:"relative", top:"175px"}}>
+          <div className="card-email bg-white d-flex flex-row flex-wrap align-items-center justify-content-between" style={{position:"relative", top:"155px"}}>
             <h5 id="send">Envío de Email</h5>
             <div className="d-flex flex-row align-items-center">
               <h5 id="deactive">Desactivar</h5>
