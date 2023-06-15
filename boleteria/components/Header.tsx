de6af200eef36137bc140f8beeeaf4a380c0df57
Maria-Chacon <123408968+Maria-Chacon@users.noreply.github.com>
2023-06-15 01:11:27 -0600

    Origin and destination

--- boleteria/components/Header.tsx
+++ boleteria/components/Header.tsx
@@ -1,47 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useContext, ChangeEvent, FormEvent } from "react";
 import { BsSearch, BsGeoFill, BsPersonFillAdd } from "react-icons/bs";
 import "react-datepicker/dist/react-datepicker.css";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider, DatePicker } from "@mui/x-date-pickers";
 import dayjs, { Dayjs } from "dayjs";
 import "dayjs/locale/es";
-import Swal from 'sweetalert2';
+import Swal from "sweetalert2";
+import { RoutesContext } from "../src/contexts/RoutesContext";
 
 import placesOrigin from "../public/placesOrigin.json";
 import placesDestination from "../public/placesDestination.json";
 import cantPerson from "../public/cantPerson.json";
 
 const Header = () => {
-  const [selectedDate, setSelectedDate] = useState<Dayjs>(dayjs());
-  const [showAlert, setShowAlert] = useState(false);
+  const {
+    selectedOrigin,
+    selectedDestination,
+    selectedDate,
+    selectedPassengers,
+    setOrigin,
+    setDestination,
+    setDate,
+    setPassengers,
+  } = useContext(RoutesContext)!;
 
+  const [showAlert, setShowAlert] = useState(false);
   const currentDate = dayjs(new Date());
 
   const handleDateChange = (date: Dayjs | null) => {
     if (date) {
-      setSelectedDate(date);
+      setDate(date.toDate());
+    }else{
+      setDate(null);
     }
   };
 
-  const handleSearchClick = () => {
-    const origin = document.getElementById("origin") as HTMLSelectElement;
-    const destination = document.getElementById(
-      "destination"
-    ) as HTMLSelectElement;
-    const passengers = document.getElementById(
-      "passengers"
-    ) as HTMLSelectElement;
+  const handleSearchClick = (e: FormEvent) => {
+    e.preventDefault();
 
     if (
-      !origin.value ||
-      !destination.value ||
-      !passengers.value ||
+      !selectedOrigin ||
+      !selectedDestination ||
+      !selectedPassengers ||
       !selectedDate
     ) {
       Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'Debe llenar todos los campos',
+        icon: "error",
+        title: "Oops...",
+        text: "Debe llenar todos los campos",
       });
     } else {
       window.location.href = "routeCalendar";
@@ -67,6 +73,10 @@ const Header = () => {
                       id="origin"
                       className="form-select"
                       aria-label="Select origin"
+                      value={selectedOrigin}
+                      onChange={(e: ChangeEvent<HTMLSelectElement>) =>
+                        setOrigin(e.target.value)
+                      }
                     >
                       <option value="">Origen</option>
                       {placesOrigin.map((origin) => (
@@ -89,6 +99,10 @@ const Header = () => {
                       id="destination"
                       className="form-select"
                       aria-label="Select destination"
+                      value={selectedDestination}
+                      onChange={(e: ChangeEvent<HTMLSelectElement>) =>
+                        setDestination(e.target.value)
+                      }
                     >
                       <option value="">Destino</option>
                       {placesDestination.map((destination) => (
@@ -111,6 +125,10 @@ const Header = () => {
                       id="passengers"
                       className="form-select"
                       aria-label="Select passenger"
+                      value={selectedPassengers}
+                      onChange={(e: ChangeEvent<HTMLSelectElement>) =>
+                        setPassengers(e.target.value)
+                      }
                     >
                       <option value="">Pasajeros</option>
                       {cantPerson.map((cant) => (
@@ -132,7 +150,7 @@ const Header = () => {
                           value={selectedDate}
                           onChange={handleDateChange}
                           format="DD-MM-YYYY"
-                          minDate={currentDate}
+                          minDate={currentDate.toDate()}
                         />
                       </div>
                     </LocalizationProvider>
