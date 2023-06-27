cb24a441da979685303e3e728c7e789dc4f162f2
AndreyBV5 <127549686+AndreyBV5@users.noreply.github.com>
2023-06-27 14:37:33 -0600

    Revert "Revert "Update Tickets.tsx""
    This reverts commit c316e5b7b904d51808d29673543e28d4dcfecb8c.

--- boleteria/components/componentsMyTickets/Tickets.tsx
+++ boleteria/components/componentsMyTickets/Tickets.tsx
@@ -1,36 +1,56 @@
-import React from "react";
-import { Button } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { getFirestore, collection, getDocs, DocumentData } from "firebase/firestore";
+import { doc, getDoc } from "firebase/firestore";
 
-const Tickets = () => {
-  const availableTickets = [
-    {
-      hourI: "10:00 AM",
-      Route: "CANOAS-SAN ISIDRO",
-      Date: "05-04-2023",
-      Precio: "5500",
-    },
-    {
-      hourI: "01:30 PM",
-      Route: "CANOAS-SAN JOSE",
-      Date: "17-07-2023",
-      Precio: "11500",
-    },
-    {
-      hourI: "08:00 AM",
-      Route: "SAN JOSE-CANOAS",
-      Date: "15-02-2023",
-      Precio: "11500",
-    },
-  ];
+import { app } from "../../src/fireBase/app";
+
+const firestore = getFirestore(app);
+
+interface TicketsProps {
+  passengerName: string;
+  seatNumber: number;
+  origin: string;
+  destination: string;
+  dateTravel: string;
+  departureTime: string;
+  arrivalTime: string;
+  hours: string;
+}
+
+const Tickets: React.FC<TicketsProps> = ({ passengerName, seatNumber, origin, destination, departureTime, arrivalTime, hours }) => {
+  const [availableTicket, setAvailableTicket] = useState<DocumentData[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const avaibletickets = collection(firestore, "dataTickets");
+        const snapshot = await getDocs(avaibletickets);
+        const tickets: DocumentData[] = await Promise.all(
+          snapshot.docs.map(async (docSnapshot) => {
+            const data = docSnapshot.data();
+
+            return data;
+          })
+        );
+
+        setAvailableTicket(tickets);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, [passengerName, seatNumber, origin, destination, departureTime, arrivalTime, hours]);
 
   return (
     <div
       className="d-flex justify-content-between align-items-center flex-column"
-      style={{ marginTop: "-50px", marginBottom: '20px'}}
+      style={{ marginTop: "-50px", marginBottom: "20px" }}
     >
-      {availableTickets.map((tick, index) => (
+      {availableTicket.map((ticket: DocumentData, index: number) => (
         <div
-          className="card mt-5 text-center" key={index}
+          className="card mt-5 text-center"
+          key={index}
           style={{
             borderRadius: "10px",
             width: "100%",
@@ -39,18 +59,20 @@ const Tickets = () => {
             color: "white",
           }}
         >
-          <div className="card-body mb-2" style={{marginTop: "25px"}}>
+          <div className="card-body mb-2" style={{ marginTop: "25px" }}>
             <h5 className="card-title mb-3">UNA-TRANSPORTE UNA</h5>
-            <h5 className="card-subtitle mb-3">{tick.Route}</h5>
-            <h5 className="card-text mb-3">Fecha salida {tick.Date}</h5>
-            <h5 className="card-text">Hora salida {tick.hourI}</h5>
-            <h5 className="card-text">Precio: {tick.Precio}</h5>
+            <h5 className="card-title mb-3">Nombre de pasajero: {ticket.passengerName}</h5>
+            <h5 className="card-title mb-3">Número de Asiento: {ticket.seatNumber}</h5>
+            <h5 className="card-subtitle mb-3">
+              Origen y destino: {ticket.origin} - {ticket.destination}
+            </h5>
+            <h5 className="card-text mb-3">Fecha de salida: {ticket.dateTravel}</h5>
+            <h5 className="card-text mb-3">Hora de salida: {ticket.departureTime}</h5>
+            <h5 className="card-text mb-3">Hora de llegada: {ticket.arrivalTime}</h5>
+            <h5 className="card-text mb-3">Duracion del viaje: {ticket.hours}</h5>
+            <h5 className="card-text">Precio Total:</h5>
             <div style={{ marginTop: "25px" }}>
-              <a
-                className="btn-tickes"
-                href="#"
-                style={{ marginRight: "25px" }}
-              >
+              <a className="btn-tickes" href="#" style={{ marginRight: "25px" }}>
                 Imprimir
               </a>
               <a className="btn-tickes" href="myTicketsQR">
@@ -63,5 +85,6 @@ const Tickets = () => {
     </div>
   );
 };
+
 
 export default Tickets;
