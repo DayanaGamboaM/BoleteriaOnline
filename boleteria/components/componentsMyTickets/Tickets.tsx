b82c9fd273bffbedcf96fd1cf8927b8147af9938
AndreyBV5 <127549686+AndreyBV5@users.noreply.github.com>
2023-06-27 14:31:33 -0600

    update tickets

--- boleteria/components/componentsMyTickets/Tickets.tsx
+++ boleteria/components/componentsMyTickets/Tickets.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { Button } from "react-bootstrap";
 import { getFirestore, collection, getDocs, DocumentData } from "firebase/firestore";
+import { doc, getDoc } from "firebase/firestore";
+
 import { app } from "../../src/fireBase/app";
 
 const firestore = getFirestore(app);
@@ -10,18 +11,28 @@ interface TicketsProps {
   seatNumber: number;
   origin: string;
   destination: string;
+  dateTravel: string;
+  departureTime: string;
+  arrivalTime: string;
+  hours: string;
 }
 
-const Tickets:React.FC<TicketsProps> = ({ passengerName, seatNumber, origin, destination }) => {
+const Tickets: React.FC<TicketsProps> = ({ passengerName, seatNumber, origin, destination, departureTime, arrivalTime, hours }) => {
   const [availableTicket, setAvailableTicket] = useState<DocumentData[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const firestore = getFirestore(app);
       try {
-        const avaibletickets = collection(firestore, "tickets");
+        const avaibletickets = collection(firestore, "dataTickets");
         const snapshot = await getDocs(avaibletickets);
-        const tickets: DocumentData[] = snapshot.docs.map((doc) => doc.data());
+        const tickets: DocumentData[] = await Promise.all(
+          snapshot.docs.map(async (docSnapshot) => {
+            const data = docSnapshot.data();
+
+            return data;
+          })
+        );
+
         setAvailableTicket(tickets);
       } catch (error) {
         console.log(error);
@@ -29,16 +40,17 @@ const Tickets:React.FC<TicketsProps> = ({ passengerName, seatNumber, origin, des
     };
 
     fetchData();
-  }, [passengerName, seatNumber, origin, destination]);
+  }, [passengerName, seatNumber, origin, destination, departureTime, arrivalTime, hours]);
 
   return (
     <div
       className="d-flex justify-content-between align-items-center flex-column"
-      style={{ marginTop: "-50px", marginBottom: '20px'}}
+      style={{ marginTop: "-50px", marginBottom: "20px" }}
     >
-      {availableTicket.map((tick: DocumentData, index: number) => (
+      {availableTicket.map((ticket: DocumentData, index: number) => (
         <div
-          className="card mt-5 text-center" key={index}
+          className="card mt-5 text-center"
+          key={index}
           style={{
             borderRadius: "10px",
             width: "100%",
@@ -47,21 +59,20 @@ const Tickets:React.FC<TicketsProps> = ({ passengerName, seatNumber, origin, des
             color: "white",
           }}
         >
-          <div className="card-body mb-2" style={{marginTop: "25px"}}>
+          <div className="card-body mb-2" style={{ marginTop: "25px" }}>
             <h5 className="card-title mb-3">UNA-TRANSPORTE UNA</h5>
-            <h5 className="card-title mb-3">Nombre de pasajero: {tick.passengerName}</h5>
-            <h5 className="card-title mb-3">Número de Asiento: {tick.seatNumber}</h5>
-            <h5 className="card-subtitle mb-3">{tick.origin}-{tick.destination}</h5>
-            <h5 className="card-text mb-3">Fecha de salida: {tick.dateTravel}</h5>
-            <h5 className="card-text mb-3">Hora de salida: {tick.departureTime}</h5>
-            <h5 className="card-text mb-3">Hora de salida: {tick.arrivalTime}</h5>
-            <h5 className="card-text">Precio Total: {tick.totalAmount}</h5>
+            <h5 className="card-title mb-3">Nombre de pasajero: {ticket.passengerName}</h5>
+            <h5 className="card-title mb-3">Número de Asiento: {ticket.seatNumber}</h5>
+            <h5 className="card-subtitle mb-3">
+              Origen y destino: {ticket.origin} - {ticket.destination}
+            </h5>
+            <h5 className="card-text mb-3">Fecha de salida: {ticket.dateTravel}</h5>
+            <h5 className="card-text mb-3">Hora de salida: {ticket.departureTime}</h5>
+            <h5 className="card-text mb-3">Hora de llegada: {ticket.arrivalTime}</h5>
+            <h5 className="card-text mb-3">Duracion del viaje: {ticket.hours}</h5>
+            <h5 className="card-text">Precio Total:</h5>
             <div style={{ marginTop: "25px" }}>
-              <a
-                className="btn-tickes"
-                href="#"
-                style={{ marginRight: "25px" }}
-              >
+              <a className="btn-tickes" href="#" style={{ marginRight: "25px" }}>
                 Imprimir
               </a>
               <a className="btn-tickes" href="myTicketsQR">
@@ -74,5 +85,6 @@ const Tickets:React.FC<TicketsProps> = ({ passengerName, seatNumber, origin, des
     </div>
   );
 };
+
 
 export default Tickets;
