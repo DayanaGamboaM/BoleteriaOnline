7e980d700e3b6953e23ad9229672c11e7fad0c40
DayanaGamboaM <mongeg1715@gmail.com>
2023-06-29 00:44:27 -0600

    seats update

--- boleteria/components/componentsPaymentGateway/Seats.tsx
+++ boleteria/components/componentsPaymentGateway/Seats.tsx
@@ -5,21 +5,34 @@ import Seat from "/public/seat.png";
 
 const Seats = () => {
   const [showSeats, setShowSeats] = useState(false);
+  const [selectedSeats, setSelectedSeats] = useState<number[]>([]);
+  const [availableSeats, setAvailableSeats] = useState<number[]>([
+    1, 2, 3, 4, 5, 14, 16, 18, 23, 27, 32, 36, 39, 43, 47, 48, 50, 52
+  ]); 
+
+  const totalSeats = 53; 
 
   const showBoxSeating = () => {
     setShowSeats(true);
   };
 
-  const availableSeats = [1, 2, 3, 4, 5, 14, 16, 18, 23, 27, 32, 36, 39, 43, 47, 48, 50, 52]; // Ejemplo: Asientos disponibles
+  const handleSeatClick = (seatNumber: number) => {
+    if (selectedSeats.includes(seatNumber)) {
+      setSelectedSeats(selectedSeats.filter((seat) => seat !== seatNumber));
+    } else if (selectedSeats.length < 5 && availableSeats.includes(seatNumber)) {
+      setSelectedSeats([...selectedSeats, seatNumber]);
+    }
+  };
 
-  const totalSeats = 53; // Total de asientos
+  const handleSeatsClose = () => {
+    setAvailableSeats(availableSeats.filter((seat) => !selectedSeats.includes(seat)));
+    setSelectedSeats([]);
+    setShowSeats(false);
+  };
 
   return (
     <div className="container mt-5">
-      <div
-        className="card text-black "
-        style={{ backgroundColor: "#D9D9D9", maxWidth: "35rem" }}
-      >
+      <div className="card text-black" style={{ backgroundColor: "#D9D9D9", maxWidth: "35rem" }}>
         <div className="card-header">
           <div className="d-flex align-items-center mr-3">
             <span className="cuadro-azul text-white mr-5">4</span>
@@ -30,14 +43,12 @@ const Seats = () => {
           <h5 className="card-title">Selecciona tus asientos. Máximo 5</h5>
 
           <button className="btn1" onClick={showBoxSeating}>
-            <Image
-              src={Seat}
-              alt="Logo de asiento"
-              className="mr-3"
-              width={35}
-            />
+            <Image src={Seat} alt="Logo de asiento" className="mr-3" width={35} />
             Seleccionar espacios
           </button>
+          {selectedSeats.length > 0 && (
+            <span className="selected-seats">Asientos: {selectedSeats.join(", ")}</span>
+          )}
         </div>
       </div>
 
@@ -46,26 +57,23 @@ const Seats = () => {
           <h3>Asientos Disponibles</h3>
           <div className="fila-asientos">
             <div className="columna-asientos">
-              {Array.from({ length: totalSeats }, (_, index) => index + 1).map(
-                (seatNumber) => (
-                  <button
-                    key={seatNumber}
-                    className="asiento"
-                    style={{
-                      borderColor: availableSeats.includes(seatNumber)
-                        ? 'green'
-                        : 'red',
-                    }}
-                    disabled={!availableSeats.includes(seatNumber)}
-                  >
-                    {seatNumber}
-                  </button>
-                )
-              )}
+              {Array.from({ length: totalSeats }, (_, index) => index + 1).map((seatNumber: number) => (
+                <button
+                  key={seatNumber}
+                  className={`asiento ${selectedSeats.includes(seatNumber) ? "selected" : ""}`}
+                  style={{
+                    border: `2px solid ${selectedSeats.includes(seatNumber) ? "red" : availableSeats.includes(seatNumber) ? "green" : "red"}`,
+                  }}
+                  disabled={!availableSeats.includes(seatNumber) || selectedSeats.length === 5}
+                  onClick={() => handleSeatClick(seatNumber)}
+                >
+                  {seatNumber}
+                </button>
+              ))}
             </div>
           </div>
           <div className="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 text-center mt-5">
-            <a className="principalButton" onClick={() => setShowSeats(false)}>
+            <a className="principalButton" onClick={handleSeatsClose}>
               <BsDoorClosed /> Cerrar
             </a>
           </div>
